c98bb61151251356bcf81ef00f5463ff0eb4ef4f
Andy <108961433+AndyPecotche@users.noreply.github.com>
2024-05-29 23:15:58 -0300

    Update mpi-sinGatters.c
    aun no funca con 8

--- mpi-sinGatters.c
+++ mpi-sinGatters.c
@@ -9,9 +9,9 @@ void combinar(int left, int medio, int right, int *ar);
 double dwalltime();
 static inline int min(int n1, int n2);
 
-long N;
+int N;
 int num_procs;
-int elementos_por_proc;
+int elementos_por_proc, elementos_por_recibir;
 int resultado = 0;
 int rank;
 int *aux_local, *a_local, *b_local; //SON GLOBALES, NO COMPARTIDAS
@@ -71,7 +71,6 @@ int main(int argc, char*argv[]){
     }
     
     N = atol(argv[1]);
-
     elementos_por_proc = N / num_procs;
     int num_procs_bk = num_procs;
 
@@ -85,12 +84,11 @@ int main(int argc, char*argv[]){
     b_local = (int*)malloc(sizeof(int) * n_local);
     aux_local = (int*)malloc(sizeof(int) * n_local);
     
-    int *a_local_bk, *b_local_bk;
+    int *a_bk, *b_bk;
     int * a, *b;
     if (rank == 0) {
         a = (int*)malloc(sizeof(int) * N);
         b = (int*)malloc(sizeof(int) * N);
-
         for (i = 0; i < N; i++) {
             b[i] = i;
             aux_local[i] = 0;
@@ -108,8 +106,8 @@ int main(int argc, char*argv[]){
         ordenarPar(L, L + 1, b_local);
     }
 
-
     for (int lenTrabajo = 4; lenTrabajo <= N; lenTrabajo *= 2) {
+        
         for (int L = 0; L < elementos_por_proc; L += lenTrabajo) {
             int M = L + lenTrabajo / 2 - 1;
             int R = min(L + lenTrabajo - 1, N - 1);
@@ -118,32 +116,54 @@ int main(int argc, char*argv[]){
         }
         
         if (lenTrabajo>=elementos_por_proc && elementos_por_proc < N){
+            if (rank != 0){ //el 0 solo recibe, no envia
+       //         printf("rank %d envia a %d, la cantidad de %d elementos\n", rank, rank/2, elementos_por_proc);
+       //         for (int i = 0; i < elementos_por_proc; i++) {
+      //              printf("RANK: %d SEND to RANK%d A_LOCAL[%d/%d]=%d \n",rank,rank/2,i,elementos_por_proc, a_local[i]);
+     //           }
+                MPI_Send(a_local, elementos_por_proc, MPI_INT, rank/2, 0, MPI_COMM_WORLD);
+                MPI_Send(b_local, elementos_por_proc, MPI_INT, rank/2, 0, MPI_COMM_WORLD);
+            }
+            elementos_por_recibir = elementos_por_proc;
             elementos_por_proc*=2;
-            if(elementos_por_proc <= n_local && rank != 0){ // solo entran los hilos que todavia tienen trabajo
-                MPI_Send(a, elementos_por_proc, MPI_INT, rank-1, MPI_ANY_TAG, MPI_COMM_WORLD); //SE PISAN LOS ARREGLOS viejos o QUE ONDA??
+            
+            if(elementos_por_proc <= n_local ){ // si el proceso tiene espacio para recibir elementos_por_proc*2 entonces entra
+                if (rank == 0) {
+                    MPI_Recv (&a_local[elementos_por_recibir], elementos_por_recibir, MPI_INT, 1 ,MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // 0 recibe siempre de 1
+                    MPI_Recv (&b_local[elementos_por_recibir], elementos_por_recibir, MPI_INT, 1 ,MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+           //         for (int i = 0; i < n_local; i++) {
+         //               printf("RANK: %d RECIEVE FROM RANK%d A_LOCAL[%d] queda: =%d \n",rank,1,i, a_local[i]);
+       //             }
+                }else{
+                    MPI_Recv (a_local, elementos_por_recibir, MPI_INT, rank*2 , MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    MPI_Recv (b_local, elementos_por_recibir, MPI_INT, rank*2 , MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+     //               for (int i = 0; i < n_local; i++) {
+ //                       printf("RANK: %d RECIEVE FROM RANK%d A_LOCAL[%d] queda: =%d \n",rank,rank*2,i, a_local[i]);
+   //                 }
+                    MPI_Recv (&a_local[elementos_por_recibir], elementos_por_recibir, MPI_INT, rank*2+1 , MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    MPI_Recv (&b_local[elementos_por_recibir], elementos_por_recibir, MPI_INT, rank*2+1 , MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//                    for (int i = 0; i < n_local; i++) {
+//                        printf("RANK: %d RECIEVE FROM RANK%d A_LOCAL[%d] queda: =%d \n",rank,rank*2+1,i, a_local[i]);
+//                    }
+                }
             }else{ //No tengo mas trabajo que hacer.
-              if(rank != 0) break; // el 0 siempre tiene trabajo que hacer.
+//                printf("RANK %d NO TIENE MAS TRABAJO QUE HACER SALGO!\n", rank);
+                break; // el 0 siempre tiene trabajo que hacer.
             }
-            
-            MPI_Recv (a_local, elementos_por_proc, MPI_INT, rank+1 , MPI_ANY_TAG, MPI_COMM_WORLD); //recibo de mi vecino
-            //deberia escribir los datos nuevos que recibo en "a" acá?
-            
-            if (elementos_por_proc > N && rank == 0){
-                    a_local_bk = a_local;
-                    b_local_bk = b_local;
-                    a_local = a; //al arreglo a le falta el ultimo combinar (lo hace rank0)
-                    b_local = b;
-            }
+
         }
 
     }
     
-    num_procs = num_procs_bk;
     elementos_por_proc = N / num_procs;
 
     if (rank == 0) {
-        a_local = a_local_bk; //recupera la direc de a_local
-        b_local = b_local_bk;
+        a_bk = a; //guarda la direccion de memoria de a
+        a = a_local; //cambia a por a_local que ahora es el arreglo a pero ordenado
+        a_local = a_bk; //a_local ahora va a ser el anterior espacio de a
+        b_bk = b;
+        b = b_local;
+        b_local = b_bk;
     }
 
     MPI_Scatter(a, elementos_por_proc, MPI_INT, a_local, elementos_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
