463bd2baf4eb1e2526adaa98d71d8c998e25e20e
Andy <108961433+AndyPecotche@users.noreply.github.com>
2024-05-30 21:11:34 -0300

    Update mpi.c

--- mpi.c
+++ mpi.c
@@ -9,9 +9,9 @@ void combinar(int left, int medio, int right, int *ar);
 double dwalltime();
 static inline int min(int n1, int n2);
 
-long N;
+int N;
 int num_procs;
-int elementos_por_proc;
+int elementos_por_proc, elementos_por_recibir;
 int resultado = 0;
 int rank;
 int *aux_local, *a_local, *b_local; //SON GLOBALES, NO COMPARTIDAS
@@ -56,6 +56,7 @@ void combinar(int left, int medio, int right, int *ar){
 
 int main(int argc, char*argv[]){
     int i, n_local;
+    int cont=1;
     double timetick;
 
     MPI_Init(&argc, &argv);
@@ -71,102 +72,87 @@ int main(int argc, char*argv[]){
     }
     
     N = atol(argv[1]);
-
     elementos_por_proc = N / num_procs;
     int num_procs_bk = num_procs;
 
     if ( rank == 0){
         n_local = N;
     }else{
-        n_local = N / pow(2, (int)log2(rank + 2));
+        for (int i=num_procs/2; i>0; i/=2){
+            if (rank % i == 0){
+                n_local = N / (num_procs/i);
+                break;
+            }
+        }
+
     }
 
     a_local = (int*)malloc(sizeof(int) * n_local);
     b_local = (int*)malloc(sizeof(int) * n_local);
     aux_local = (int*)malloc(sizeof(int) * n_local);
-    int *sendcounts;
-    int *displs;
-    int *a_local_bk, *b_local_bk;
+    
+    int *a_bk, *b_bk;
     int * a, *b;
     if (rank == 0) {
-        sendcounts = (int*)malloc(sizeof(int) * num_procs);
-        displs = (int*)malloc(sizeof(int) * num_procs);
         a = (int*)malloc(sizeof(int) * N);
         b = (int*)malloc(sizeof(int) * N);
-
         for (i = 0; i < N; i++) {
             b[i] = i;
             aux_local[i] = 0;
             a[i] = (N - 1) - i;
         }
-        for (i = 0; i < num_procs; i++) {
-            sendcounts[i] = elementos_por_proc;
-            displs[i] = i * elementos_por_proc;
-        }
         timetick = dwalltime(); //rank 0 mide el tiempo
     }
     
-    MPI_Scatterv(a,sendcounts,displs, MPI_INT, a_local, elementos_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatterv(b,sendcounts,displs, MPI_INT, b_local, elementos_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
+    //cambiar por scatter normal
+    MPI_Scatter(a, elementos_por_proc, MPI_INT, a_local, elementos_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(b, elementos_por_proc, MPI_INT, b_local, elementos_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
 
     for (int L = 0; L < elementos_por_proc; L += 2) {
         ordenarPar(L, L + 1, a_local);
         ordenarPar(L, L + 1, b_local);
     }
 
-
     for (int lenTrabajo = 4; lenTrabajo <= N; lenTrabajo *= 2) {
-      if (elementos_por_proc <= n_local && elementos_por_proc != 0) {  
+        
         for (int L = 0; L < elementos_por_proc; L += lenTrabajo) {
             int M = L + lenTrabajo / 2 - 1;
             int R = min(L + lenTrabajo - 1, N - 1);
             combinar(L, M, R, a_local);
             combinar(L, M, R, b_local);
         }
-      }
+        
         if (lenTrabajo>=elementos_por_proc && elementos_por_proc < N){
-            MPI_Gatherv(a_local, elementos_por_proc, MPI_INT, a, sendcounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
-            MPI_Gatherv(b_local, elementos_por_proc, MPI_INT, b, sendcounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
+            elementos_por_recibir = elementos_por_proc;
             elementos_por_proc*=2;
-            if (rank == 0 && elementos_por_proc < N) {
-                num_procs = num_procs / 2;
-                for (i = 0; i < num_procs; i++) {
-                    sendcounts[i] = elementos_por_proc;
-                    displs[i] = i * elementos_por_proc;
-                }
-                for (i = num_procs; i < num_procs_bk; i++) {
-                    sendcounts[i] = 0;
-                    displs[i] = 0;
-                }
-            }
-            if (elementos_por_proc < N){
-                if (elementos_por_proc > n_local){
-                    elementos_por_proc = 0;
-                }
-                MPI_Scatterv(a,sendcounts,displs, MPI_INT, a_local, elementos_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
-                MPI_Scatterv(b,sendcounts,displs, MPI_INT, b_local, elementos_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
+            if (elementos_por_proc > n_local){
+                MPI_Send(a_local, elementos_por_recibir, MPI_INT, rank-cont, 0, MPI_COMM_WORLD);
+                // for (int i = 0; i < elementos_por_recibir; i++) {
+                //     printf("RANK: %d SEND to RANK%d A_LOCAL[%d/%d]=%d \n",rank,rank-cont,i,elementos_por_recibir-1,a_local[i]);
+                // }
+                MPI_Send(b_local, elementos_por_recibir, MPI_INT, rank-cont, 0, MPI_COMM_WORLD);
+                break;
             }else{
-                if (rank == 0) {
-                    a_local_bk = a_local;
-                    b_local_bk = b_local;
-                    a_local = a; //al arreglo a le falta el ultimo combinar (lo hace rank0)
-                    b_local = b;
-                }
+         // si el proceso tiene espacio para recibir elementos_por_proc*2 entonces entra para recibir trabajo
+                MPI_Recv (&a_local[elementos_por_recibir], elementos_por_recibir, MPI_INT, rank+cont ,MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // 0 recibe siempre de 1
+                MPI_Recv (&b_local[elementos_por_recibir], elementos_por_recibir, MPI_INT, rank+cont ,MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    // for (int i = 0; i < n_local; i++) {
+                    //     printf("RANK: %d RECIEVE FROM RANK%d A_LOCAL[%d] queda: =%d \n",rank,1,i, a_local[i]);
+                    // }
             }
+        cont*=2;
         }
-
     }
     
-    num_procs = num_procs_bk;
     elementos_por_proc = N / num_procs;
 
     if (rank == 0) {
-        a_local = a_local_bk; //recupera la direc de a_local
-        b_local = b_local_bk;
-        for (i = 0; i < num_procs; i++) {
-            sendcounts[i] = elementos_por_proc;
-            displs[i] = i * elementos_por_proc;
-        }
+        a_bk = a; //guarda la direccion de memoria de a
+        a = a_local; //cambia a por a_local que ahora es el arreglo a pero ordenado
+        a_local = a_bk; //a_local ahora va a ser el anterior espacio de a
+        b_bk = b;
+        b = b_local;
+        b_local = b_bk;
     }
 
     MPI_Scatter(a, elementos_por_proc, MPI_INT, a_local, elementos_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
